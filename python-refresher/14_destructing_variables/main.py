a2f82bf09eb06916a6a44f35f8320b8de53ca7d4
Pedro Paulo Aguiar <pedroa-guiar@outlook.com>
2023-06-27 01:07:37 -0300

    Adicionando conteudo

---
+++ python-refresher/14_destructing_variables/main.py
@@ -0,0 +1,19 @@
+people = [("Bob", 42, "Mechanic"), ("James", 24, "Artist"), ("Harry", 32, "Lecturer")]
+
+for name, age, profession in people:
+    print(f"Name: {name}, Age: {age}, Profession: {profession}")
+
+
+# Ignorando variavel
+person = ("Bob", 42, "Mechanic")
+name, _, profession = person
+print(name, profession)
+
+# Acumulando mais de um valor na segunda variável
+head, *tail = [1, 2, 3, 4, 5]
+print(head)
+print(tail)
+
+*head, tail = [1, 2, 3, 4, 5]
+print(head)
+print(tail)
