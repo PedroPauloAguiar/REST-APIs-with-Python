49797ba4c4176ac1a3faf562d5ddf8f11244ddd7
Pedro Paulo Aguiar <pedroa-guiar@outlook.com>
2023-06-27 01:52:10 -0300

    Adicionando conteudo

---
+++ python-refresher/16_functions_arguments_parameters/main.py
@@ -0,0 +1,13 @@
+def add(x, y):
+    result = x + y
+    print(result)
+
+
+add(5, 3)
+
+
+def say_hello(name, surname):
+    print(f"Hello, {name} {surname}")
+
+
+say_hello(surname="Bob", name="Smith")
---
+++ python-refresher/17_default_paramaters_values/main.py
@@ -0,0 +1,17 @@
+def add(x, y=8):
+    result = x + y
+    print(result)
+
+
+add(5)
+
+
+default_y = 3
+
+
+def addd(x, y=default_y):
+    sum = x + y
+    print(sum)
+
+
+addd(2)
