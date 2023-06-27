65ce168ac4e32677b393d4c05da6095f79a5d4ca
Pedro Paulo Aguiar <pedroa-guiar@outlook.com>
2023-06-27 18:11:15 -0300

    Adicionando conteudo

---
+++ python-refresher/27_class_composition/main.py
@@ -0,0 +1,20 @@
+class BookShelf:
+    def __init__(self, *books):
+        self.books = books
+
+    def __str__(self):
+        return f"BookShelf with {len(self.books)} books"
+
+
+class Book:
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return f"Book {self.name}"
+
+
+book = Book("Harry Potter")
+book2 = Book("Python 101")
+shelf = BookShelf(book, book2)
+print(shelf)
