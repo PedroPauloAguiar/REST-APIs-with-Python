e2f02b3bcda7041bf3aee4763b86fb3696aec676
Pedro Paulo Aguiar <pedroa-guiar@outlook.com>
2023-06-27 17:00:59 -0300

    Adicionando conteudo

---
+++ python-refresher/25_classmethod_staticmethod/main.py
@@ -0,0 +1,45 @@
+# Exemplo 1
+class ClassTest:
+    def instance_method(self):
+        print(f"Called instance_method of {self}")
+
+    @classmethod
+    def class_method(cls):
+        print(f"Called class_method of {cls}")
+
+    @staticmethod
+    def static_method():
+        print("Called static method")
+
+
+# ClassTest.static_method()
+
+
+# Exemplo 2
+
+
+class Book:
+    TYPES = ("hardcover", "paperback")
+
+    def __init__(self, name, book_type, weight):
+        self.name = name
+        self.book_type = book_type
+        self.weight = weight
+
+    def __repr__(self):
+        return f"<Book {self.name}, {self.book_type}, weighing {self.weight}g>"
+
+    @classmethod
+    def hardcover(cls, name, page_weight):
+        return cls(name, cls.TYPES[0], page_weight + 100)
+
+    @classmethod
+    def paperback(cls, name, page_weight):
+        return cls(name, cls.TYPES[1], page_weight)
+
+
+book = Book.hardcover("Harry Potter", 1500)
+light = Book.paperback("Python 101", 600)
+
+print(book)
+print(light)
