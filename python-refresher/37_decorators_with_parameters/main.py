2b226dd570e9470a2664a2cdad5aac11dd848d84
Pedro Paulo Aguiar <pedroa-guiar@outlook.com>
2023-06-28 00:13:01 -0300

    Adicionando material

---
+++ python-refresher/37_decorators_with_parameters/main.py
@@ -0,0 +1,36 @@
+import functools
+
+user = {"username": "jose", "acess_level": "guest"}
+
+
+def make_secure(access_level):
+    def decorator(func):
+        @functools.wraps(func)
+        def secure_function(*args, **kwargs):
+            if user["acess_level"] == "acces_level":
+                return func(*args, **kwargs)
+            else:
+                return f"No {access_level} permissions for {user['username']}"
+
+        return secure_function
+
+    return decorator
+
+
+@make_secure("admin")
+def get_admin_password():
+    return "admin 1234"
+
+
+@make_secure("user")
+def get_dashboard_password():
+    return "user: user_password"
+
+
+print(get_admin_password())
+print(get_dashboard_password())
+
+user = {"username": "anna", "acess_level": "admin"}
+
+print(get_admin_password())
+print(get_dashboard_password())
